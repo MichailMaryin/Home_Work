247b7dd6f99e5fd1ec0cf06f5e1de77c140d0962
Michail <sobol996@gmail.com>
2022-11-20 20:36:43 +0300

    Home_Work_3.1

---
+++ HW_3/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.Write("Введите Число N = ");
+int num = int.Parse(Console.ReadLine());
+if (num % 2 == 1 )
+{
+    Console.Write("Нет, не Четное");
+}
+else
+{
+       Console.Write("Да, Четное");
+}
