6a6722d36e8300d462ae0daa51b47c14a102eb4a
Michail <sobol996@gmail.com>
2022-11-20 20:37:00 +0300

    Home_Work_2.1

---
+++ HW_2/Program.cs
@@ -0,0 +1,36 @@
+﻿Console.Clear();
+Console.Write("Inter Number A = ");
+int numA = int.Parse(Console.ReadLine());
+Console.Write("Inter Number B = ");
+int numB = int.Parse(Console.ReadLine());
+Console.Write("Inter Number C = ");
+int numC = int.Parse(Console.ReadLine());
+int max = numA;
+
+ if (max < numB)
+    {
+        if (numB > numC)
+        {
+        Console.Write("Number B Больше = ");
+        Console.WriteLine(numB);
+        }
+        else
+        {
+            Console.Write("Number C Больше = ");
+            Console.WriteLine(numC);
+        }
+    }
+    else
+    {
+        if (max < numC)
+        {
+            Console.Write("Number C Больше = ");
+            Console.WriteLine(numC);  
+        }
+        else
+        {
+            Console.Write("Number A Больше = ");
+            Console.WriteLine(numA);  
+        }
+    }
+
