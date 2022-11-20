28f0d303734c5a29f651242d46daccfc48256d7f
Michail <sobol996@gmail.com>
2022-11-20 20:31:30 +0300

    Home_Work_4.1

--- HW_4/Program.cs
+++ HW_4/Program.cs
@@ -3,16 +3,15 @@ Console.Clear();
 Console.Write("Введите Число N = ");
 int num = int.Parse(Console.ReadLine());
 //int start = 1;
-
+int i = 1; // 1 - не четные числа ; 0 - Четные
 
 for (int start = 1;start <= num ;start++)
 {
-    if (start % 2 == 0)
+    if (start % 2 == i)
     {
         Console.WriteLine(start);
     }
 }
-
 
 //while (start < num + 1)
 //{
