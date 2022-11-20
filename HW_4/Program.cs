24605fe1cd6aff0f39ce2228c3901a889e215075
Michail <sobol996@gmail.com>
2022-11-20 20:26:15 +0300

    Home_Work_4

---
+++ HW_4/Program.cs
@@ -0,0 +1,28 @@
+﻿
+Console.Clear();
+Console.Write("Введите Число N = ");
+int num = int.Parse(Console.ReadLine());
+//int start = 1;
+
+
+for (int start = 1;start <= num ;start++)
+{
+    if (start % 2 == 0)
+    {
+        Console.WriteLine(start);
+    }
+}
+
+
+//while (start < num + 1)
+//{
+//    if (start % 2 == 1)
+//    {
+//        start++;
+//    }
+//    else
+//    {
+//        Console.WriteLine(start);
+//        start++;
+//    }   
+//}
