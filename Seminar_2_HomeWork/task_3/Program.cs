a75c41ac902d5908fdfdec267314edccd07cd715
Michail <sobol996@gmail.com>
2022-11-21 23:04:23 +0300

    task_3.1

---
+++ Seminar_2_HomeWork/task_3/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.Write("Введите число - ");
+int num = int.Parse(Console.ReadLine());
+int day6 = 6;
+int day7 = 7;
+
+if (num == day6 || day7 == num)
+{
+    Console.Write("Является ");
+}
+else
+{
+    Console.Write("Не является");
+}
