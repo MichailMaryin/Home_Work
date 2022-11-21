f2a708382451aea71848198c2d8ebbb37e85571d
Michail <sobol996@gmail.com>
2022-11-21 22:11:48 +0300

    task_2

---
+++ Seminar_2_HomeWork/task_2/Program.cs
@@ -0,0 +1,20 @@
+﻿int number = int.Parse(Console.ReadLine());
+
+if (number < 100)
+{
+    System.Console.WriteLine("неправильный ввод");
+    return 0;
+}
+
+int num = number;
+
+for (int i = 10; num > 999;)
+{
+    num /= i;
+}
+
+System.Console.WriteLine(num%10);
+
+return 0;
+
+
