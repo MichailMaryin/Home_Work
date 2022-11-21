309bf6b97dd463e185e2ff417279016a455b520b
Michail <sobol996@gmail.com>
2022-11-21 22:31:52 +0300

    task_3

---
+++ Seminar_2_HomeWork/task_1/Program.cs
@@ -0,0 +1,9 @@
+﻿Console.Clear();
+
+Console.Write("Ведите число ");
+
+string num = Console.ReadLine();
+
+Console.Write("Вторая цифра - ");
+//               Обращаемся к массиву строки.
+Console.WriteLine(num[1].ToString());
