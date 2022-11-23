3e6b2584b1816f8717443c6bb77a719626cd57ef
Michail <sobol996@gmail.com>
2022-11-23 16:46:01 +0300

    task_3

---
+++ Seminar_3_HomeWork/task_3/Program.cs
@@ -0,0 +1,9 @@
+﻿Console.Clear();
+Console.Write("Введите N = ");
+int num = int.Parse(Console.ReadLine());
+
+for(int ind = 1;ind < num + 1; ind++)
+    {
+        double number = Math.Pow(ind, 3); //ind * ind * ind;
+        Console.WriteLine(number);
+    }
