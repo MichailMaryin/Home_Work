da7672b3771da78825694162da9c3b58a73f423d
Michail <sobol996@gmail.com>
2022-11-23 16:58:50 +0300

    task_3

---
+++ Seminar_3_HomeWork/task_1/Program.cs
@@ -0,0 +1,22 @@
+﻿Console.WriteLine("на вход пятизначное число = ");
+string num = Console.ReadLine();
+
+if (num[0].ToString() == num[4].ToString())
+{
+    if (num[1].ToString() == num[3].ToString())
+    {
+         Console.WriteLine("является палиндромом");
+    } 
+    else
+    {
+        {
+            Console.WriteLine("не является палиндромом");
+        }
+    }    
+}
+else
+{
+    {
+        Console.WriteLine("не является палиндромом");
+    }
+}
