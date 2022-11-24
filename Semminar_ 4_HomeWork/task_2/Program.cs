f3e5edc316e72ade66d0a71a3a742ac578187cb9
Michail <sobol996@gmail.com>
2022-11-24 20:21:08 +0300

    task_2

---
+++ Semminar_ 4_HomeWork/task_2/Program.cs
@@ -0,0 +1,17 @@
+﻿// Подсчитать сумму цифр в числе
+Console.Clear();
+
+Console.WriteLine("Введите Число = ");
+string num =Console.ReadLine();
+int size =num.Length;
+int index = 0;
+int x = 0;
+
+while (index < size)
+{
+    int a = int.Parse(num[index].ToString());
+    x = x + a;
+    index++;
+}
+Console.Write("итог = ");
+Console.WriteLine(x);
