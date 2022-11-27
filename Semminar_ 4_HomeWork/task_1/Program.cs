ace1f02b55096372d49ed20418fe76c87ced79db
Michail <sobol996@gmail.com>
2022-11-27 15:39:09 +0300

    Task_1.4

---
+++ Semminar_ 4_HomeWork/task_1/Program.cs
@@ -0,0 +1,9 @@
+﻿Console.Clear();
+Console.Write("Enter A = ");
+int a = int.Parse(Console.ReadLine());
+Console.Write("Enter B = ");
+int b = int.Parse(Console.ReadLine());
+
+double c =Convert.ToInt64(Math.Pow(a, b));
+Console.Write("Итог = ");
+Console.WriteLine(c);
