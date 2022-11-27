8d89373e4f2fa4bfd48ce99958718f984c51861a
Michail <sobol996@gmail.com>
2022-11-27 15:34:38 +0300

    Task_3.4

---
+++ Semminar_ 4_HomeWork/task_3/Program.cs
@@ -0,0 +1,26 @@
+﻿
+void FillArrayRandomNumbers(int[] array)
+
+{
+        for (int i = 0; i < array.Length; i++)
+    {
+        array[i] = new Random().Next(1, 10);
+    }
+}
+
+void PrintArray( int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write(array[i] + " ");
+    }
+    Console.WriteLine();
+}
+ 
+Console.Clear();
+
+int[] a =new int[7];
+
+FillArrayRandomNumbers(a);
+
+PrintArray(a);
