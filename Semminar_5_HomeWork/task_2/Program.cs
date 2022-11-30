e068761e0d36f0dca991f4b9f72910e93d68e7c3
Michail <sobol996@gmail.com>
2022-12-01 01:14:04 +0300

    task_2.5

---
+++ Semminar_5_HomeWork/task_2/Program.cs
@@ -0,0 +1,46 @@
+﻿/*Задайте одномерный массив, заполненный случайными числами. Найдите сумму элементов, стоящих на нечётных позициях.
+
+[3, 7, 23, 12] -> 19
+
+[-4, -6, 89, 6] -> 0*/
+
+
+void FillArrayRandomNumbers(int[] array)
+
+{
+    for (int i = 0; i < array.Length; i++)
+     {
+        array[i] = new Random().Next(1, 10);
+     }   
+}
+
+void PrintArray(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write(array[i] + " ");
+    }
+    Console.WriteLine();
+}
+
+void FindSumEvenNumber(int[] array)
+{
+    int sum = 0;
+    for(int i= 0;i<array.Length;i++)
+    {
+        if (array[i]%2== 1)
+        {
+            sum = sum + array[i];
+        }
+    }
+    Console.WriteLine(sum);
+}
+
+
+int[] array =new int[4];
+
+FillArrayRandomNumbers(array);
+
+PrintArray(array);
+
+FindSumEvenNumber(array);
